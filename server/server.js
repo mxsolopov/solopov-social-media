608326265944b739b8848f9db1cd314645a90164
Maksim Solopov <mxsolopov@yandex.ru>
2023-03-03 16:21:11 +0300

    Test hosting 2

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 import path from "path"
 import express from "express"
 import mongoose from "mongoose"
-import template from "./../template"
+// import template from "./../template"
 import config from "./../config/config"
 import app from "./express"
 // comment out before building for production
@@ -14,7 +14,8 @@ const CURRENT_WORKING_DIR = process.cwd()
 app.use("/dist", express.static(path.join(CURRENT_WORKING_DIR, "dist")))
 
 app.get("/", (req, res) => {
-  res.status(200).send(template())
+  // res.status(200).send(template())
+  res.status(200).type("html").send(html)
 })
 
 app.listen(config.port, (err) => {
@@ -31,3 +32,22 @@ mongoose.connect(config.mongoUri, {})
 mongoose.connection.on("error", () => {
   throw new Error(`unable to connect to database: ${config.mongoUri}`)
 })
+
+const html = `<!doctype html>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Solopov Social Network</title>
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+      integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
+      crossorigin="anonymous"
+    />
+  </head>
+  <body>
+    <div id="root"></div>
+    <script src="/dist/bundle.js"></script>
+  </body>
+</html>`
